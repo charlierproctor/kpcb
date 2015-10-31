a08b4938bdb16578fc9de8e762897f4c8907d982
Charlie Proctor <charlie@charlieproctor.com>
2015-10-31 00:53:10 -0400

    working on the hash.h... adding function prototypes

--- hash.h
+++ hash.h
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct node {
-	char *str;
-	void *obj;
-	struct node *next;
-};
+typedef struct node {
+	char *str;					// the key of this element
+	const void *obj;			// the arbitrary object reference
+	struct node *next;			// next element the linked list
+} Node;
 
-struct hash {
+typedef struct hash {
+	Node **table;			// the hash table itself.
 	size_t size;			// size of the hash table
 	size_t num_elements;  	// number of elements in the hash table
+} Hash;
 
-};
+Hash *hashCreate(size_t size);
 
+bool hashSet(Hash *hash, const char *key, const void *value);
+
+void *hashGet(Hash *hash, const char *key);
+
+void *hashDelete(Hash *hash, const char *key);
+
+float *hashLoad(Hash *hash);
